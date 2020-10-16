987808335ddace81ede75abb3fda57be928f1fc4
benkm <kitchingmorleyb@gmail.com>
2020-10-16 14:34:48 +0100

    Added documentation

--- publication_results.py
+++ publication_results.py
@@ -4,9 +4,28 @@ from tqdm import tqdm
 from bayesian_functions import *
 
 
-## Part I : Producing p-values for figure 4.
-def part1():
-  run_bootstrap = False
+def get_pvalues_central_fit():
+  """
+    This function will reproduce the p-value data for the central fit as
+    defined in the publication
+
+    INPUTS :
+    --------
+
+    OUTPUTS:
+    --------
+    pvalues_N2: (2, ) list of arrays of floats, containing the pvalues for the
+      N = 2 analysis. Each array is of the length of the number of GL_min cut
+      values, and the corresponding p-value to each cut is recorded. The first
+      array is for the Lambda_IR = g / (4 pi N) model, while the second is for
+      Lambda_IR = 1 / L
+
+    pvalues_N4: (2, ) list of arrays of floats, containing the pvalues for the
+      N = 4 analysis. Each array is of the length of the number of GL_min cut
+      values, and the corresponding p-value to each cut is recorded. The first
+      array is for the Lambda_IR = g / (4 pi N) model, while the second is for
+      Lambda_IR = 1 / L      
+  """
   GL_mins = numpy.array([0.8, 1.6, 2.4, 3.2, 4, 4.8, 6.4, 8, 9.6, 12.8, 14.4, 16, 19.2, 24, 25.6, 28.8, 32])
   GL_max = 76.8
 
@@ -27,6 +46,8 @@ def part1():
     pvalues_1[i], params1, dof = run_frequentist_analysis(model1, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False)
     pvalues_2[i], params2, dof = run_frequentist_analysis(model2, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False)
 
+  pvalues_N2 = [pvalues_1, pvalues_2]
+
   # N = 4
   N = 4
   Bbar_1 = "0.420"
@@ -44,9 +65,37 @@ def part1():
     pvalues_1[i], params1, dof = run_frequentist_analysis(model1, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False)
     pvalues_2[i], params2, dof = run_frequentist_analysis(model2, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False)
 
+  pvalues_N4 = [pvalues_1, pvalues_2]
 
-## Part II: Producing parameter estimates and statistical error for central fit
-def part2():
+  return pvalues_N2, pvalues_N4
+
+
+
+def get_statistical_errors_central_fit():
+  """
+    This function gets the statistical error bands (and central fit values)
+    for the model parameters, and the value of the critical mass at g=0.1 quoted
+    quoted in the publication.
+
+    INPUTS :
+    --------
+
+    OUTPUTS :
+    ---------
+    results_N2: list of the form [params, params_std, m_c, m_c_error]
+      where params are the parameter estimates of the central fit, params_std
+      are the statistical errors on these estimates, found through bootstrap
+      resampling, m_c is the critcal mass, and m_c_error is the statistical
+      error on this
+
+    results_N4: list of the form
+      [params, params_std, m_c1, m_c1_error, m_c2, m_c2_error],
+      where params are the parameter estimates of the central fit, params_std
+      are the statistical errors on these estimates, found through bootstrap
+      resampling, m_c1 is the critcal mass estimated using the first alpha value,
+      and m_c_error is the statistical error on this. Similarly for m_c2 and
+      m_c2_error for the second alpha value
+  """
   # N = 2
   model = model1_1a
   N = 2
@@ -58,41 +107,130 @@ def part2():
   x0 = [0, 0.5431, -0.03586, 1, 2 / 3] # EFT values
   param_names = ["alpha", "f0", "f1", "lambduh", "nu"]
 
-  pvalue, params1, dof, sigmas = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0)
+  # Run once with the full dataset (no resampling)
+  pvalue, params, dof, sigmas = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False)
 
+  # Run with all the bootstrap samples
+  pvalue_boot, params_boot, dof_boot, sigmas_boot = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0)
 
+  # Calculate the value of the non-perterbative critical mass for g = 0.1 and it's
+  # statistical error
+  g = 0.1
+  m_c = mPT_1loop(g, N) + g ** 2 * (params[0] - params[-2] * K1(g, N))
+  print(f"m_c = {m_c}")
+
+  alphas = params_boot[..., 0]
+  lambduhs = params_boot[..., -2]
+
+  m_cs = mPT_1loop(g, N) + g ** 2 * (alphas - lambduhs * K1(g, N))
+
+  minimum_m = numpy.min(m_cs)
+  maximum_m = numpy.max(m_cs)
+
+  print(f"m_c_range = {[minimum_m, maximum_m]}")
+
+  m_c_error = max(m_c - minimum_m, maximum_m - m_c)
+  print(f"m_c_error = {m_c_error}")
+
+  results_N2 = [params, numpy.std(params_boot), m_c, m_c_error]
+  
   # N = 4
-  model = model1_2a
+  model = model1_1a
   N = 4
   GL_min = 12.8
   GL_max = 76.8
 
-  Bbar_1 = "0.420"
-  Bbar_2 = "0.430"
-  x0 = [0, 0, 0.4459, -0.02707, 1, 2 / 3] # EFT values
-  param_names = ["alpha1", "alpha2", "f0", "f1", "lambduh", "nu"]
+  Bbar_1 = "0.520"
+  Bbar_2 = "0.530"
+  x0 = [0, 0.5431, -0.03586, 1, 2 / 3] # EFT values
+  param_names = ["alpha1", "alpha2" "f0", "f1", "lambduh", "nu"]
 
-  pvalue, params1, dof, sigmas = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0)
+  # Run once with the full dataset (no resampling)
+  pvalue, params, dof, sigmas = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False)
+
+  # Run with all the bootstrap samples
+  pvalue_boot, params_boot, dof_boot, sigmas_boot = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0)
+
+  # Calculate the value of the non-perterbative critical mass for g = 0.1 and it's
+  # statistical error. Calculate twice, once for each alpha
+  g = 0.1
+  m_c1 = mPT_1loop(g, N) + g ** 2 * (params[0] - params[-2] * K1(g, N))
+  print(f"m_c1 = {m_c1}")
+
+  alphas = params_boot[..., 0]
+  lambduhs = params_boot[..., -2]
+
+  m_c1s = mPT_1loop(g, N) + g ** 2 * (alphas - lambduhs * K1(g, N))
+
+  minimum_m = numpy.min(m_c1s)
+  maximum_m = numpy.max(m_c1s)
+
+  print(f"m_c1_range = {[minimum_m, maximum_m]}")
+
+  m_c1_error = max(m_c1 - minimum_m, maximum_m - m_c1)
+  print(f"m_c1_error = {m_c1_error}")
+
+  m_c2 = mPT_1loop(g, N) + g ** 2 * (params[0] - params[-2] * K1(g, N))
+  print(f"m_c2 = {m_c2}")
+
+  alphas = params_boot[..., 0]
+  lambduhs = params_boot[..., -2]
+
+  m_c2s = mPT_1loop(g, N) + g ** 2 * (alphas - lambduhs * K1(g, N))
+
+  minimum_m = numpy.min(m_c2s)
+  maximum_m = numpy.max(m_c2s)
+
+  print(f"m_c2_range = {[minimum_m, maximum_m]}")
+
+  m_c2_error = max(m_c2 - minimum_m, maximum_m - m_c2)
+  print(f"m_c2_error = {m_c2_error}")
+
+  results_N4 = [params, numpy.std(params_boot), m_c1, m_c1_error, m_c2, m_c2_error]
+
+  return results_N2, results_N4
 
 
-## PART III: Calculating a systematic error based on different Bbar combinations
-def part3():
-  GL_mins = numpy.array([0.8, 1.6, 2.4, 3.2, 4, 4.8, 6.4, 8, 9.6, 12.8, 14.4, 16, 19.2, 24, 25.6, 28.8, 32])
-  GL_max = 76.8
 
+def get_systematic_errors():
+  """
+    This function gets the systematic error bands (and central fit values)
+    for the model parameters, and the value of the critical mass at g=0.1 quoted
+    quoted in the publication.
+
+    INPUTS :
+    --------
+
+    OUTPUTS :
+    ---------
+    results_N2: list of the form [params, params_std, m_c, m_c_error]
+      where params are the parameter estimates of the central fit, params_std
+      are the systematic errors on these estimates, found through bootstrap
+      resampling, m_c is the critcal mass, and m_c_error is the systematic
+      error on this
+
+    results_N4: list of the form
+      [params, params_std, m_c1, m_c1_error, m_c2, m_c2_error],
+      where params are the parameter estimates of the central fit, params_std
+      are the systematic errors on these estimates, found through bootstrap
+      resampling, m_c1 is the critcal mass estimated using the first alpha value,
+      and m_c_error is the systematic error on this. Similarly for m_c2 and
+      m_c2_error for the second alpha value
+  """
   GL_mins = numpy.array([0.8, 1.6, 2.4, 3.2, 4, 4.8, 6.4, 8, 9.6, 12.8, 14.4, 16, 19.2, 24, 25.6, 28.8, 32])
   GL_max = 76.8
 
   # N = 2
   N = 2
   model = model1_1a
-  min_dof = 15
+  min_dof = 15 # The minimum number of degrees of freedom needed to consider a fit valid
 
   x0 = [0, 0.5431, -0.03586, 1, 2 / 3] # EFT values
   param_names = ["alpha", "f0", "f1", "lambduh", "nu"]
   n_params = len(param_names)
   Bbar_s = ["0.510", "0.520", "0.530", "0.540", "0.550", "0.560", "0.570", "0.580", "0.590"]
 
+  # Make a list of all Bbar pairs
   Bbar_list = []
   for i in range(len(Bbar_s)):
     for j in range(i + 1, len(Bbar_s)):
@@ -109,6 +247,7 @@ def part3():
     for j, GL_min in enumerate(GL_mins):
       pvalues[i, j], params[i, j], dofs[i, j] = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False, print_info=False)
 
+  # Extract the index of the smallest GL_min fit that has an acceptable p-value
   r = len(GL_mins)
   best = r - 1
 
@@ -125,34 +264,63 @@ def part3():
   print(f"pvalue : {pvalues[best_Bbar_index, best]}")
   print(f"dof : {dofs[best_Bbar_index, best]}")
 
-  ### GET THE SYSTEMATIC FITS
+  params_central = params[best_Bbar_index, best]
+
+  # Find the parameter variation over acceptable fits
   acceptable = numpy.logical_and(
                numpy.logical_and(pvalues > 0.05, pvalues < 0.95),
                       dofs >= min_dof)
 
+  # Find the most extreme values of the parameter estimates that are deemed acceptable
   for i, param in enumerate(param_names):
     param_small = params[..., i]
     minimum = numpy.min(param_small[acceptable])
     maximum = numpy.max(param_small[acceptable])
 
+  sys_sigmas = numpy.zeros(n_params)
+
   for i, param in enumerate(param_names):
-    sys_sigma = max(maximum - params[best_Bbar_index, best, i], params[best_Bbar_index, best, i] - minimum)
+    # Define the systematic error bar by the largest deviation from the central
+    # fit by an acceptable fit
+    sys_sigmas[i] = max(maximum - params[best_Bbar_index, best, i], params[best_Bbar_index, best, i] - minimum)
 
     print(f"{param} = {param} +- {sys_sigma}")
 
-  GL_mins = numpy.array([0.8, 1.6, 2.4, 3.2, 4, 4.8, 6.4, 8, 9.6, 12.8, 14.4, 16, 19.2, 24, 25.6, 28.8, 32])
-  GL_max = 76.8
+  # Find the systematic variation in the critical mass
+  g = 0.1
+  m_c = mPT_1loop(g, N) + g ** 2 * (params[best_Bbar_index, best, 0] - params[best_Bbar_index, best, -2] * K1(g, N))
+  print(f"m_c = {m_c}")
+
+  alphas = params[..., 0]
+  lambduhs = params[..., -2]
+
+  # Only include parameter estimates from those fits that are acceptable
+  alphas = alphas[acceptable]
+  lambduhs = lambduhs[acceptable]
+
+  m_cs = mPT_1loop(g, N) + g ** 2 * (alphas - lambduhs * K1(g, N))
+
+  minimum_m = numpy.min(m_cs)
+  maximum_m = numpy.max(m_cs)
+
+  print(f"m_c_range = {[minimum_m, maximum_m]}")
+
+  m_c_error = max(m_c - minimum_m, maximum_m - m_c)
+  print(f"m_c_error = {m_c_error}")
+
+  results_N2 = [params_central, sys_sigma, m_c, m_c_error]
 
   # N = 4
   N = 4
   model = model1_2a
-  min_dof = 15
+  min_dof = 15 # The minimum number of degrees of freedom needed to consider a fit valid
 
-  x0 = [0, 0, 0.4459, -0.02707, 1, 2 / 3] # EFT values
+  x0 = [0, 0, 0.5431, -0.03586, 1, 2 / 3] # EFT values
   param_names = ["alpha1", "alpha2", "f0", "f1", "lambduh", "nu"]
   n_params = len(param_names)
   Bbar_s = ["0.420", "0.430", "0.440", "0.450", "0.460", "0.470"]
 
+  # Make a list of all Bbar pairs
   Bbar_list = []
   for i in range(len(Bbar_s)):
     for j in range(i + 1, len(Bbar_s)):
@@ -169,6 +337,7 @@ def part3():
     for j, GL_min in enumerate(GL_mins):
       pvalues[i, j], params[i, j], dofs[i, j] = run_frequentist_analysis(model, N, Bbar_1, Bbar_2, GL_min, GL_max, param_names, x0, run_bootstrap=False, print_info=False)
 
+  # Extract the index of the smallest GL_min fit that has an acceptable p-value
   r = len(GL_mins)
   best = r - 1
 
@@ -185,20 +354,74 @@ def part3():
   print(f"pvalue : {pvalues[best_Bbar_index, best]}")
   print(f"dof : {dofs[best_Bbar_index, best]}")
 
-  ### GET THE SYSTEMATIC FITS
+  params_central = params[best_Bbar_index, best]
+
+  # Find the parameter variation over acceptable fits
   acceptable = numpy.logical_and(
                numpy.logical_and(pvalues > 0.05, pvalues < 0.95),
                       dofs >= min_dof)
 
+  # Find the most extreme values of the parameter estimates that are deemed acceptable
   for i, param in enumerate(param_names):
     param_small = params[..., i]
     minimum = numpy.min(param_small[acceptable])
     maximum = numpy.max(param_small[acceptable])
 
+  sys_sigmas = numpy.zeros(n_params)
+
   for i, param in enumerate(param_names):
-    sys_sigma = max(maximum - params[best_Bbar_index, best, i], params[best_Bbar_index, best, i] - minimum)
+    # Define the systematic error bar by the largest deviation from the central
+    # fit by an acceptable fit
+    sys_sigmas[i] = max(maximum - params[best_Bbar_index, best, i], params[best_Bbar_index, best, i] - minimum)
 
     print(f"{param} = {param} +- {sys_sigma}")
+
+  # Find the systematic variation in the critical mass
+  g = 0.1
+
+  # Calculate using alpha1
+  m_c1 = mPT_1loop(g, N) + g ** 2 * (params[best_Bbar_index, best, 0] - params[best_Bbar_index, best, -2] * K1(g, N))
+  print(f"m_c1 = {m_c1}")
+
+  alphas = params[..., 0]
+  lambduhs = params[..., -2]
+
+  # Only include parameter estimates from those fits that are acceptable
+  alphas = alphas[acceptable]
+  lambduhs = lambduhs[acceptable]
+
+  m_c1s = mPT_1loop(g, N) + g ** 2 * (alphas - lambduhs * K1(g, N))
+
+  minimum_m = numpy.min(m_c1s)
+  maximum_m = numpy.max(m_c1s)
+
+  print(f"m_c1_range = {[minimum_m, maximum_m]}")
+
+  m_c1_error = max(m_c1 - minimum_m, maximum_m - m_c1)
+  print(f"m_c1_error = {m_c1_error}")
+
+  # Calculate using alpha2
+  m_c2 = mPT_1loop(g, N) + g ** 2 * (params[best_Bbar_index, best, 0] - params[best_Bbar_index, best, -2] * K1(g, N))
+  print(f"m_c2 = {m_c2}")
+
+  alphas = params[..., 0]
+  lambduhs = params[..., -2]
+
+  # Only include parameter estimates from those fits that are acceptable
+  alphas = alphas[acceptable]
+  lambduhs = lambduhs[acceptable]
+
+  m_c2s = mPT_1loop(g, N) + g ** 2 * (alphas - lambduhs * K1(g, N))
+
+  minimum_m = numpy.min(m_c2s)
+  maximum_m = numpy.max(m_c2s)
+
+  print(f"m_c2_range = {[minimum_m, maximum_m]}")
+
+  m_c2_error = max(m_c2 - minimum_m, maximum_m - m_c2)
+  print(f"m_c2_error = {m_c2_error}")
+
+  results_N4 = [params_central, sys_sigma, m_c1, m_c1_error, m_c2, m_c2_error]
 
 
 ## Part IV: Calculating the Bayes Factor for figure 4.
